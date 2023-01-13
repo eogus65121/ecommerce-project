d6b5a2d4f1933ffc3c9d9092f14973d0b4cda7c1
eogus65121 <eogus6512@gmail.com>
2023-01-13 21:14:47 +0900

    feat : Users entity 생성

---
+++ src/main/java/com/done/ecommerce/domain/entity/Users.java
@@ -0,0 +1,42 @@
+package com.done.ecommerce.domain.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String user_id;
+
+    @Column(nullable = false)
+    private String user_pwd;
+
+    private int age;
+
+    private String phone;
+
+    @Column(nullable = false)
+    private String role;
+
+    @Builder
+    public Users(String name, String user_id, String user_pwd, String role){
+        this.name = name;
+        this.user_id = user_id;
+        this.user_pwd = user_pwd;
+        this.role = role;
+    }
+}
