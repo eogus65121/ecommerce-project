aa2672c9034b07c35d1cce7c9c6078b6794ad9a4
eogus65121 <eogus6512@gmail.com>
2023-01-24 21:04:34 +0900

    feat : user_id로 조회 기능 추가

--- src/main/java/com/done/ecommerce/domain/repository/UserRepository.java
+++ src/main/java/com/done/ecommerce/domain/repository/UserRepository.java
@@ -5,9 +5,15 @@ import com.done.ecommerce.dto.users.UserDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<Users, Long>{
 
+    // id와 pwd 일치 여부로 로그인 check
     UserDto findByIdAndPwd(String id, String pwd);
 
+    Optional<Users> findByUser_id(String id);
+
+    void merge(Users user) throws Exception;
 }
