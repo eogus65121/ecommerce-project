a955bb57e9dac6f6cc2a73f676226ae005cf9e9c
eogus65121 <eogus6512@gmail.com>
2023-01-31 22:15:56 +0900

    feat : Products Entity 객체 대신 결과값을 받을 ProductsDto 추가

---
+++ src/main/java/com/done/ecommerce/dto/products/ProductsDto.java
@@ -0,0 +1,27 @@
+package com.done.ecommerce.dto.products;
+
+
+import com.done.ecommerce.domain.entity.Products;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonAutoDetect
+public class ProductsDto {
+    private Long id;
+    private String name;
+    private String description;
+    private int price;
+    private LocalDate createdDt;
+    private String createUsrId;
+    private int groupId;
+    private String remark;
+}
