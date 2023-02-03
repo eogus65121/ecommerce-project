af67f39ac60121de5de7d29b20539a7542a627f2
eogus65121 <eogus6512@gmail.com>
2023-02-03 21:17:17 +0900

    fix : sessionUtil 오타 수정 - id -> name 변경 - session 관리 데이터 사용자 휴대폰 번호 추가

--- src/main/java/com/done/ecommerce/utils/SessionUtil.java
+++ src/main/java/com/done/ecommerce/utils/SessionUtil.java
@@ -9,6 +9,7 @@ public class SessionUtil {
     private static final String LOGIN_USER_NAME = "LOGIN_USER_NAME";
     private static final String LOGIN_USER_ID = "LOGIN_USER_ID";
     private static final String LOGIN_USER_ROLE = "LOGIN_USER_ROLE";
+    private static final String LOGIN_USER_PHONE ="LOGIN_USER_PHONE";
 
     // 인스턴스화 방지
     private SessionUtil(){}
@@ -17,14 +18,15 @@ public class SessionUtil {
         SessionUtil.setLoginUserRole(session, userDto.getRole());
         SessionUtil.setLoginUserName(session, userDto.getName());
         SessionUtil.setLoginUserId(session, userDto.getUserId());
+        SessionUtil.setLoginUserPhone(session, userDto.getPhone());
     }
 
     public static String getLoginUserName(HttpSession session){
-        return session.getAttribute(LOGIN_USER_ID).toString();
+        return session.getAttribute(LOGIN_USER_NAME).toString();
     }
 
     public static void setLoginUserName(HttpSession session, String name){
-        session.setAttribute(LOGIN_USER_ID, name);
+        session.setAttribute(LOGIN_USER_NAME, name);
     }
 
 
@@ -43,6 +45,14 @@ public class SessionUtil {
     // Enum을 사용하여 로그인 사용자의 Role을 세션에 저장
     public static void setLoginUserRole(HttpSession session, int role){
         session.setAttribute(LOGIN_USER_ROLE, Role.findRole(role));
+    }
+
+    public static String getLoginUserPhone(HttpSession session){
+        return session.getAttribute(LOGIN_USER_PHONE).toString();
+    }
+
+    public static void setLoginUserPhone(HttpSession session, String phone){
+        session.setAttribute(LOGIN_USER_PHONE, phone);
     }
 
     public static void sessionClear(HttpSession session){
