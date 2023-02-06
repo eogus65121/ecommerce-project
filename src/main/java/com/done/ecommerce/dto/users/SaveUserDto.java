ebeef80ec1fb20315339517c0e7603e62e6b5b97
eogus65121 <eogus6512@gmail.com>
2023-02-06 23:34:50 +0900

    feat : 회원가입 시 pwd에 대한 pattern과 phone에 대한 pattern 추가

--- src/main/java/com/done/ecommerce/dto/users/SaveUserDto.java
+++ src/main/java/com/done/ecommerce/dto/users/SaveUserDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Getter @Setter
 public class SaveUserDto {
@@ -14,8 +15,12 @@ public class SaveUserDto {
     @NotBlank
     private String userId;
     @NotBlank
+    @Pattern(message = "최소 한 개 이상의 대소문자와 숫자, 특수문자를 포함한 8자 이상 16자 이하의 비밀번호를 입력해야합니다.",
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#!~$%^&-+=()])(?=\\S+$).{8,16}$")
     private String userPwd;
     @NotBlank
+    @Pattern(message="전화번호는 숫자만 입력이 가능합니다.",
+            regexp = "^[0-9]+$")
     private String phone;
     private int role;
 
