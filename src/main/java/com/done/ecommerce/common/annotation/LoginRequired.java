34b3402792dc396546f4f06ab4fe0ee339ca7d8a
eogus65121 <eogus6512@gmail.com>
2023-02-12 22:36:05 +0900

    chore : 커스텀 어노테이션에 대한 주석 추가

--- src/main/java/com/done/ecommerce/common/annotation/LoginRequired.java
+++ src/main/java/com/done/ecommerce/common/annotation/LoginRequired.java
@@ -2,6 +2,10 @@ package com.done.ecommerce.common.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 로그인이 된 사용자만 접근 가능한 URL에 대한 확인
+ */
+
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
--- src/main/java/com/done/ecommerce/common/annotation/LoginUser.java
+++ src/main/java/com/done/ecommerce/common/annotation/LoginUser.java
@@ -5,6 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * 로그인 완료된 사용자의 정보 가져오기
+ */
+
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.PARAMETER})
 public @interface LoginUser {
