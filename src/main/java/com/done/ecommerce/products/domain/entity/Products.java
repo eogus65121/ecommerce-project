dfaf08eae1381f3a54d7fe2aa245bd7f1dba109a
eogus65121 <eogus6512@gmail.com>
2023-03-01 23:31:05 +0900

    fix : product entity groupId > category로 변경 및 category entity와 JoinColumn 추가

--- src/main/java/com/done/ecommerce/products/domain/entity/Products.java
+++ src/main/java/com/done/ecommerce/products/domain/entity/Products.java
@@ -17,6 +17,7 @@ public class Products extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)     // auto increment
+    @Column(name = "PRODUCT_ID")
     private Long id;
 
     @Column(length = 500, nullable = false)
@@ -34,18 +35,20 @@ public class Products extends BaseTimeEntity {
     @Column(nullable = false)
     private String createUsrId;         // 등록자
 
-    private int groupId;        // 상품 그룹 코드
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "CATEGORY_ID")
+    private Category category;        // 상품 그룹 코드
 
     private String remark;          // 비고
 
     @Builder
-    public Products(String name, String description, int price, LocalDate createdDt, String createUsrId, int groupId, String remark){
+    public Products(String name, String description, int price, LocalDate createdDt, String createUsrId, Category category, String remark){
         this.name = name;
         this.description = description;
         this.price = price;
         this.createdDt = createdDt;
         this.createUsrId = createUsrId;
-        this.groupId = groupId;
+        this.category = category;
         this.remark = remark;
     }
 
